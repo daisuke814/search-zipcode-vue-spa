ed55ad5cf6693f5133cca7ccee4294e8abf24eb3
阪本大将 <>
2022-12-09 14:58:28 +0900

    郵便番号から住所を検索するSPA

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-text-field
+      density="compact"
+      variant="solo"
+      label="郵便番号を検索"
+      prepend-inner-icon="mdi-magnify"
+      v-model="zipcode"
+      @keydown.enter="submitZipcode"
+      placeholder="郵便番号を入力してください"
+      single-line
+      hide-details
+  ></v-text-field>
+</template>
+
+<script>
+
+import Router from "@/router";
+
+export default {
+  name: 'SearchForm',
+  data() {
+    return {
+    }
+  },
+  methods: {
+    submitZipcode() {
+      Router.push({
+        name: 'detail',
+        params: {
+          zipcode: this.zipcode
+        }
+      });
+    }
+  },
+  mounted() {
+    this.zipcode = this.$route.params.zipcode;
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row style="height: 100%;" align="center" justify="center">
+    <v-col cols="4">
+      <h1 class="mb-2 text-center text-white"><span style="color: red;">〒</span> 郵便番号から住所を検索</h1>
+      <SearchForm></SearchForm>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+import SearchForm from "@/components/SearchForm";
+
+export default {
+  name: "HomeView",
+  components: {
+    SearchForm
+  },
+}
+</script>
